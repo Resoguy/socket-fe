ef60a2ae35fb8a36330f16268c27d6d2d68e98c0
Resit Korsu <resitkorsu@gmail.com>
2021-06-05 14:14:05 +0300

    add mouse event

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import s from './Home.module.css';
 
-const Home = () => (
-    <div>
-        <h2>Home Page!</h2>
-    </div>
-);
+class Home extends React.Component {
+    state = {
+        client: {
+            x: 0,
+            y: 0
+        }
+    }
+
+
+    componentDidMount() {
+        document.addEventListener('mousemove', this.handleMousemove)
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mousemove', this.handleMousemove);
+    }
+
+    handleMousemove = event => {
+        const {clientX, clientY} = event;
+
+        console.log({x: clientX, y: clientY});
+
+        this.setState({client: {x: clientX, y: clientY}});
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>HomePage!</h1>
+                <div style={{top: this.state.client.y, left: this.state.client.x}} className={s.box}>
+                    <img src={`https://unsplash.it/${this.state.client.x}/${this.state.client.y}`} />
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Home;
